98f00aa06e855ca5bac65f98517a7b3753aa0027
sama <samashalabi2018@gmail.com>
2026-03-24 02:38:43 +0200

    fix formating

--- src/types/request_content.ts
+++ src/types/request_content.ts
@@ -1,6 +1,6 @@
-export type SendHttpRequestPayload={
-    url:string;
-    httpMethod: "POST" | "GET" | "DELETE" | "PUT";
-    headers?:Record<string,string>;
-    body?:unknown;
-}
+export type SendHttpRequestPayload = {
+  url: string;
+  httpMethod: "POST" | "GET" | "DELETE" | "PUT";
+  headers?: Record<string, string>;
+  body?: unknown;
+};
